a3963fb356d47208f59431521f9dc57bd24a5b11
Danillo Silva <danillordm@gmail.com>
2022-06-19 23:02:11 -0300

    :lipstick: @Lillow [Add CSS in ArrowDown with animation]

--- src/components/ArrowDown/index.js
+++ src/components/ArrowDown/index.js
@@ -1,6 +1,11 @@
-function ArrowDown() {
+import "./style.css";
+
+function ArrowDown(currentPlayer = 1) {
   return /*html*/ `
-        <img src="assets/images/IconArrowDown.png" alt="Ícone de seta para baixo">
+       <img class="arrow-down"
+            data-currentPlayer="${currentPlayer}" 
+            src="assets/images/IconArrowDown.png" 
+            alt="Ícone de seta para baixo">
     `;
 }
 
